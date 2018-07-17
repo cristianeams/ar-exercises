72bab6f7441ce0e5fae1cc0debbb4d9635d29246
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-07-17 23:24:07 +0000

    Implemented validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "-- Add a new store --"
+puts "Please enter a store name:"
+puts "> "
+@new_store_name = gets.chomp
+puts "Your store name is: #{@new_store_name}"
+
+@new_store = Store.create(name: "#{@new_store_name}")
+if @new_store.errors.any? # If there are errors, do something
+    @new_store.errors.each do |attribute, message| # do stuff for each error
+      puts "The #{attribute} you entered #{message}"
+    end
+  end
